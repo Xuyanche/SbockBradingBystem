630e8f67a905f887a5f109b03a6406ceffa1e2ac
徐延成 <3160101851@zju.edu.cn>
2019-04-26 21:56:59 +0800

    Update finSQLGen.java

--- src/finSys/finSQLGen.java
+++ src/finSys/finSQLGen.java
@@ -3,27 +3,71 @@ import java.util.Scanner;
 
 public class finSQLGen {
 	static String financeTableName = "financeTable";
-	static String IDColumnName = "financeID";
-	
+	static String IDColumnName = "FinID";
+	static String pwdColumnName = "password";
+	static String balanceColumnName = "balance";
+	static String stateColumnName = "state";
 	
 	public static void main() {
 		Scanner input = new Scanner(System.in);
 		String read = input.next();
 		
-		System.out.println(sqlGenerateSearch(read));
+		System.out.println(sqlGenSearch(read));
 		return;
 	}
 
+	////////////////////////////////////
+		
 	
 	
-	
-	public static String sqlGenerateSearch(String FinanceID) {
+	public static String sqlGenSearch(String FinID) {
 		String sqlselect = " select " + "*";
 		String sqlfrom = " from " + financeTableName;
-		String sqlwhere =  " where " + IDColumnName + " = " + FinanceID + ";";
+		String sqlwhere =  " where " + IDColumnName + " = " + FinID + ";";
 		
 		
 		return sqlselect + sqlfrom + sqlwhere;
 	}
 	
+	
+	
+	public static String sqlGenUpdatepwd(String FinID, String newpwd) {
+		String sqlwhere =  " where " + IDColumnName + " = " + FinID + ";";
+		return "update " + pwdColumnName + " = " + newpwd + sqlwhere;
+	}
+	
+
+	public static String sqlGenCheckpwd(String FinID, String pwd) {
+		String sqlselect = " select " + pwdColumnName;
+		String sqlfrom = " from " + financeTableName;
+		String sqlwhere =  " where " + IDColumnName + " = " + FinID + ";";
+		
+		return sqlselect + sqlfrom + sqlwhere;
+	}
+	
+
+	public static String sqlGenUpateBalance(String FinID, String amount) {
+		String sqlwhere =  " where " + IDColumnName + " = " + FinID + ";";
+		
+		return "update " + balanceColumnName + " = " + balanceColumnName + " + " + amount + sqlwhere;
+	}
+	
+	
+	public static String sqlGenNewAccount(String FinID, String SecID, String pwd, String balance) {
+		String sqlValues = " values ( " 
+				+ FinID + " , " + SecID + " , " + pwd + " , " + balance + " , " + "true" + " ); ";
+		
+		return "insert into "+ financeTableName + sqlValues;
+	}
+	
+	
+	public static String sqlGenLock(String FinID) {
+		String sqlwhere =  " where " + IDColumnName + " = " + FinID + ";";
+		return "udpate " + stateColumnName + " = false " + sqlwhere;
+	}
+	
+	public static String sqlGenUnlock(String FinID) {
+		String sqlwhere =  " where " + IDColumnName + " = " + FinID + ";";
+		return "udpate " + stateColumnName + " = true " + sqlwhere;
+	}
 }
